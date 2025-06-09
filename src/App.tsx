7dabe05fa8c3ce66b9ba6e9fefee69c441d72bb8
Mykhailo Stratan <36572099+MykhailoStratan@users.noreply.github.com>
2025-06-08 23:12:20 -0700

    Add transparent left tool panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import ThreeScene from './ThreeScene'
+import ToolPanel from './ToolPanel'
 import './App.css'
 
 export default function App() {
   return (
     <div className="app">
+      <ToolPanel />
       <ThreeScene />
     </div>
   )
---
+++ src/ToolPanel.tsx
@@ -0,0 +1,9 @@
+import './ToolPanel.css'
+
+export default function ToolPanel() {
+  return (
+    <div className="tool-panel">
+      {/* Buttons for instruments will be added here */}
+    </div>
+  );
+}
