14acd7fd3292e4183fe23eb0784daf0c564e0ab5
Mykhailo Stratan <36572099+MykhailoStratan@users.noreply.github.com>
2025-06-08 23:40:54 -0700

    Show message when adding plane

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import ThreeScene from './ThreeScene'
 import ToolPanel from './ToolPanel'
 import './App.css'
 
 export default function App() {
   const [planes, setPlanes] = useState<number[]>([])
+  const [message, setMessage] = useState<string | null>(null)
 
   const addPlane = () => {
     setPlanes((prev) => [...prev, prev.length])
+    setMessage('Plane added')
   }
+
+  useEffect(() => {
+    if (message) {
+      const id = setTimeout(() => setMessage(null), 2000)
+      return () => clearTimeout(id)
+    }
+  }, [message])
 
   return (
     <div className="app">
       <ToolPanel onAddPlane={addPlane} />
       <ThreeScene planes={planes} />
+      {message && <div className="message">{message}</div>}
     </div>
   )
 }
