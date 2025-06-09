d008cae7d6a646c27ed77bdb992399d05556eb5f
Mykhailo Stratan <36572099+MykhailoStratan@users.noreply.github.com>
2025-06-08 23:56:31 -0700

    feat: enable object selection and translation

--- src/ThreeScene.tsx
+++ src/ThreeScene.tsx
@@ -1,21 +1,34 @@
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, TransformControls } from '@react-three/drei'
+import { useRef, useState } from 'react'
 import type { JSX } from 'react'
-import { DoubleSide } from 'three'
+import type { OrbitControls as OrbitControlsImpl } from 'three-stdlib'
+import { DoubleSide, Object3D } from 'three'
 
 
-function Box(props: JSX.IntrinsicElements['mesh']) {
+function Box({ onSelect, ...props }: JSX.IntrinsicElements['mesh'] & { onSelect: (obj: Object3D) => void }) {
+  const ref = useRef<Object3D>(null!)
   return (
-    <mesh {...props}>
+    <mesh
+      ref={ref}
+      {...props}
+      onClick={() => onSelect(ref.current)}
+    >
       <boxGeometry args={[1, 1, 1]} />
       <meshStandardMaterial color="orange" />
     </mesh>
   )
 }
 
-function Plane(props: JSX.IntrinsicElements['mesh']) {
+function Plane({ onSelect, ...props }: JSX.IntrinsicElements['mesh'] & { onSelect: (obj: Object3D) => void }) {
+  const ref = useRef<Object3D>(null!)
   return (
-    <mesh rotation={[-Math.PI / 2, 0, 0]} {...props}>
+    <mesh
+      ref={ref}
+      rotation={[-Math.PI / 2, 0, 0]}
+      {...props}
+      onClick={() => onSelect(ref.current)}
+    >
       <planeGeometry args={[10, 10]} />
       <meshStandardMaterial color="lightgray" side={DoubleSide} />
     </mesh>
@@ -26,15 +39,33 @@ interface ThreeSceneProps {
 }
 
 export default function ThreeScene({ planes }: ThreeSceneProps) {
+  const [selected, setSelected] = useState<Object3D | null>(null)
+  const orbitRef = useRef<OrbitControlsImpl | null>(null)
+
   return (
-    <Canvas style={{ height: '100vh', width: '100vw' }}>
+    <Canvas
+      style={{ height: '100vh', width: '100vw' }}
+      onPointerMissed={() => setSelected(null)}
+    >
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      <Box />
+      <Box onSelect={setSelected} />
       {planes.map((id) => (
-        <Plane key={id} position={[0, 0, 0]} />
+        <Plane key={id} position={[0, 0, 0]} onSelect={setSelected} />
       ))}
-      <OrbitControls />
+      {selected && (
+        <TransformControls
+          object={selected}
+          mode="translate"
+          onMouseDown={() => {
+            if (orbitRef.current) orbitRef.current.enabled = false
+          }}
+          onMouseUp={() => {
+            if (orbitRef.current) orbitRef.current.enabled = true
+          }}
+        />
+      )}
+      <OrbitControls ref={orbitRef} />
     </Canvas>
   )
 }
